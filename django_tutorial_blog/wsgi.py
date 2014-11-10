2bdd5c2a0d7117420505b358d1f69d6dbab2e344
ioanas78 <sdi78@yahoo.com>
2014-11-10 18:07:26 +0100

    updates to wsgi.py e settings.py

--- django_tutorial_blog/wsgi.py
+++ django_tutorial_blog/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tutorial_blog_ng.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tutorial_blog.settings")
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#application = get_wsgi_application()
 
+from dj_static import Cling
 application = Cling(get_wsgi_application())
